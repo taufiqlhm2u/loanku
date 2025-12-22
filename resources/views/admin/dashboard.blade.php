7ac2fa641a9ba0a20d2197ef310f49c47f4efe83
taufiq <cellijo724@gmail.com>
2025-12-22 12:41:18 +0700

    sidebar

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,11 +1,47 @@
 <x-layout>
     <x-slot:title>Dashboard</x-slot:title>
     @push('css')
-    <style>
-        h1 {
-            color: red;
-        }
-    </style>
+        <link rel="stylesheet" href="{{ asset('css/admin/sidebar.css') }}">
     @endpush
-    <h1 id="title">Hello</h1>
+    <div class="d-flex">
+        <!-- sidebar -->
+        <aside id="sidebar">
+            <div class="sidebar-logo">
+                <a href="">Loanku</a>
+            </div>
+            <div class="sidebar-center">
+                <ul class="sidebar-nav p-0">
+                    <li class="sidebar-item">
+                        <a href="" class="sidebar-link"><i class="ri-dashboard-fill"></i>Dashboard</a>
+                    </li>
+                    <li class="sidebar-item">
+                        <a href="" class="sidebar-link"><i class="ri-group-fill"></i>Users</a>
+                    </li>
+                    <li class="sidebar-item">
+                        <a href="" class="sidebar-link"><i class="ri-instance-fill"></i>Items</a>
+                    </li>
+                    <li class="sidebar-item">
+                        <a href="" class="sidebar-link"><i class="ri-bookmark-fill"></i>Categories</a>
+                    </li>
+                    <li class="sidebar-item">
+                        <a href="" class="sidebar-link"><i class="ri-history-fill"></i>History Loan</a>
+                    </li>
+                </ul>
+                <div class="sidebar-footer">
+                    <a href="" class="sidebar-link"><i class="ri-logout-box-fill"></i>LogOut</a>
+                </div>
+            </div>
+        </aside>
+        <!-- main -->
+        <div class="main">
+            <nav class="navbar navbar-expand">
+                Admin\Dashboard
+            </nav>
+            <main class="p-3">
+                <div class="container-fluid">
+
+                </div>
+            </main>
+        </div>
+    </div>
 </x-layout>
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,9 +8,12 @@
     <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
     <script scr="{{ asset('js/bootstrap.js') }}"></script>
     <script scr="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@4.7.0/fonts/remixicon.css" rel="stylesheet" />
     @stack('css')
 </head>
+
 <body>
     {{ $slot }}
 </body>
+
 </html>
